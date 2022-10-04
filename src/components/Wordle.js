1877e7487acf09cfd5af6031cc79fe1632fc6687
Stacey <stacey6105@gmail.com>
2022-10-04 23:54:58 +0900

    grid and row components created, basic grid formatted in css

--- src/components/Wordle.js
+++ src/components/Wordle.js
@@ -1,5 +1,6 @@
 import React, {useEffect} from "react";
 import useWordle from "../hooks/useWordle";
+import Grid from "./Grid";
 
 export default function Wordle({ solution }) {
     const { currentGuess, handleKeyup, guesses, isCorrect, turn } = useWordle(solution);
@@ -20,6 +21,7 @@ export default function Wordle({ solution }) {
             {isCorrect &&
                 <div>You are correct</div>
             }
+            <Grid currentGuess = {currentGuess} guesses = {guesses} turn = {turn} />
         </div>
     )
 }
